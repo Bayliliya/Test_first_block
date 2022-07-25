eb64a1e24b61329d3dfe9457c5a66141e1a51c1e
Liliya <gizat.83@mail.ru>
2022-07-25 09:30:04 +0300

    Подсчет количеств строк с 3-мя и меньше символами

--- TEST/Test.cs
+++ TEST/Test.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Данная программа из массива строк формирует новый массив, состоящий из строк длина которых меньше или равна 3 символам
+string [] array = {"123", "dfdggd", "fgfh", "abc", ":-)", "^_^"};
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3) count +=1;
+} 
+Console.WriteLine(count);
